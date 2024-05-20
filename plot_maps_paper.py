d14e19ec565cbd30ae795fcf1d54c1fc229188bc
Mathilde Van Cuyck <mathilde.vancuyck22@gmail.com>
2024-05-20 18:00:45 -0500

    update maps

--- plot_maps_paper.py
+++ plot_maps_paper.py
@@ -17,16 +17,15 @@ wcs_list    = []
 im_list    = []
 freq_list = []
 
-trans = 'CO10'
+trans = 'CO32'
 
-for z in (0.5, 0.5):
+for z in (0.5, 3.0):
     for data in ('', 'all_lines_', 'galaxy'):
 
         if(data=='galaxy'): 
             type = 'galaxies_pix'
         else: 
             type = f'{trans}_{data}MJy_sr' 
-
 
         file = f'outputs_cubes/pySIDES_from_uchuu_ntile_0_z{z}_dz{dz}_0.0slices_9deg2_{type}.fits'
         hdu = fits.open(file)
@@ -38,10 +37,9 @@ for z in (0.5, 0.5):
         im = hdu[0].data[0, :, :]
         im_list.append(im) 
 
-
         if(data=='galaxy'): 
             title=f'Galaxies at z={z}'
-            cmap='Greys'
+            cmap='gist_gray'
             vmin=0
             vmax=10
         else: 
@@ -62,7 +60,7 @@ axs = np.ravel(axs)
 for iax, (data, z, cmap, title, vmin, vmax) in enumerate(params_list):
     wcs = wcs_list[iax]
     freq = freq_list[iax]
-    if('Gal' in title): im = axs[iax].imshow(im_list[iax], origin='lower', cmap=cmap,)
+    if('Gal' in title): im = axs[iax].imshow(im_list[iax], origin='lower', vmax=1, cmap=cmap,)
     else: 
         im = axs[iax].imshow(1.e3*im_list[iax], origin='lower', cmap=cmap, norm=LogNorm(vmin=vmin, vmax=vmax))#, projection=wcs)    
         if(iax==1): im1 = im
@@ -70,7 +68,6 @@ for iax, (data, z, cmap, title, vmin, vmax) in enumerate(params_list):
     #if(iax==0 or iax==3): 
     axs[iax].set_ylabel('Dec')
     axs[iax].set_title(title)
-
 
 # Create colorbar for cividis colormap
 cbar_ax_cividis = fig.add_axes([0.92, 0.55, 0.02, 0.35])  # [left, bottom, width, height]
@@ -81,8 +78,9 @@ cbar_cividis.set_label('$\\rm I_{\\nu}$ [kJy/sr]')
 cbar_ax_greys = fig.add_axes([0.92, 0.15, 0.02, 0.35])  # [left, bottom, width, height]
 cbar_greys = fig.colorbar(im, cax=cbar_ax_greys)
 cbar_greys.set_label('Galaxies count')
+cbar_greys.set_ticks([0, 1])
 
 fig.tight_layout(rect=[0, 0, 0.9, 1])  # Adjust the layout to make space for the colorbars
-fig.savefig('figs/Slice_GHz.png', bbox_inches='tight', pad_inches=0)
+fig.savefig(f'figs/Slice_{freq}GHz.pdf', bbox_inches='tight', pad_inches=0)
 plt.show()
         
