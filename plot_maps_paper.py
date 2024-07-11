ad588efc3c8bdc982f257e35f5c2bf85530c6dc7
Mathilde Van Cuyck <mathilde.vancuyck22@gmail.com>
2024-07-11 12:08:40 -0500

    update

--- plot_maps_paper.py
+++ plot_maps_paper.py
@@ -10,7 +10,7 @@ import astropy.convolution as conv
 from IPython import embed
 from matplotlib.colors import LogNorm
 
-dz = 0.1 
+dz = 0.05
 
 params_list = []
 wcs_list    = []
@@ -18,6 +18,7 @@ im_list    = []
 freq_list = []
 
 trans = 'CO32'
+Trans = 'CO(3-2)'
 
 for z in (0.5, 3.0):
     for data in ('', 'all_lines_', 'galaxy'):
@@ -27,14 +28,14 @@ for z in (0.5, 3.0):
         else: 
             type = f'{trans}_{data}MJy_sr' 
 
-        file = f'outputs_cubes/pySIDES_from_uchuu_ntile_0_z{z}_dz{dz}_0.0slices_9deg2_{type}.fits'
+        file = f'outputs_cubes/pySIDES_from_uchuu_ntile_2_z{z}_dz{dz}_2.0slices_9deg2_{type}.fits'
         hdu = fits.open(file)
         wcs = WCS(hdu[0].header) 
         freq = wcs.pixel_to_world(0, 0, 0)[1].to('GHz')
         freq_list.append( freq )
         wcs = WCS(hdu[0].header, naxis=2) 
         wcs_list.append( wcs )
-        im = hdu[0].data[0, :, :]
+        im = np.sum(hdu[0].data[:, :, :], axis=0)
         im_list.append(im) 
 
         if(data=='galaxy'): 
@@ -43,8 +44,8 @@ for z in (0.5, 3.0):
             vmin=0
             vmax=10
         else: 
-            if('all' in data): title = f'{trans} and interlopers \n at (z={z},'+' $\\rm \\nu$={:0.0f})'.format(freq)
-            else: title = f'{trans} at z={z}'
+            if('all' in data): title = f'{Trans} and interlopers \n at (z={z},'+' $\\rm \\nu$={:0.0f})'.format(freq)
+            else: title = f'{Trans} at z={z}'
             cmap = 'cividis'
             vmax = 0.008*1e3
             vmin = 0.1
@@ -53,7 +54,7 @@ for z in (0.5, 3.0):
 
 BS=10; plt.rc('font', size=BS); plt.rc('axes', titlesize=BS); plt.rc('axes', labelsize=BS); lw=1; mk=5; elw=1
 fig, axs = plt.subplots(2, 3, #sharex=True, sharey = 'row', #gridspec_kw={'height_ratios': [1,1]}, 
-                        figsize=(11,8), dpi = 200,
+                        figsize=(11.2,8.2), dpi = 200,
                         subplot_kw={'projection': wcs_list[0]} )
 axs = np.ravel(axs)
 
@@ -70,17 +71,17 @@ for iax, (data, z, cmap, title, vmin, vmax) in enumerate(params_list):
     axs[iax].set_title(title)
 
 # Create colorbar for cividis colormap
-cbar_ax_cividis = fig.add_axes([0.92, 0.55, 0.02, 0.35])  # [left, bottom, width, height]
+cbar_ax_cividis = fig.add_axes([0.92, 0.45, 0.02, 0.35])  # [left, bottom, width, height]
 cbar_cividis = fig.colorbar(im1, cax=cbar_ax_cividis)
 cbar_cividis.set_label('$\\rm B_{\\nu}$ [kJy/sr]')
 
 # Create colorbar for Greys colormap
-cbar_ax_greys = fig.add_axes([0.92, 0.15, 0.02, 0.35])  # [left, bottom, width, height]
+cbar_ax_greys = fig.add_axes([0.92, 0.05, 0.02, 0.35])  # [left, bottom, width, height]
 cbar_greys = fig.colorbar(im, cax=cbar_ax_greys)
 cbar_greys.set_label('Galaxies count')
 cbar_greys.set_ticks([0, 1])
 
-fig.tight_layout(rect=[0, 0, 0.9, 1])  # Adjust the layout to make space for the colorbars
-fig.savefig(f'figs/Slice_{freq}GHz.pdf', bbox_inches='tight', pad_inches=0)
+fig.tight_layout(rect=[0, 0, 0.9, 0.8])  # Adjust the layout to make space for the colorbars
+fig.savefig(f'slices.pdf', bbox_inches='tight', pad_inches=0)
 plt.show()
         
