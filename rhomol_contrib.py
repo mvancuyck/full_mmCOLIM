ce71c27e8669d3eb2046fb824f4d69470ba27945
Mathilde Van Cuyck <mathilde.vancuyck22@gmail.com>
2024-09-18 14:56:05 -0500

    updqte

--- rhomol_contrib.py
+++ rhomol_contrib.py
@@ -34,6 +34,8 @@ for tile_sizeRA, tile_sizeDEC, Nsimu in params['tile_sizes']:
     
     bar = Bar(f'computing rho_mol(z) for {tile_sizeRA}x{tile_sizeDEC}deg2', max=Nsimu)  
 
+    tab = np.zeros(Nsimu, len(z_list), 2 )
+
     for l in range(Nsimu):
         
         cat = Table.read(params["output_path"]+f"pySIDES_from_uchuu_tile_{l}_{tile_sizeRA}deg_x_{tile_sizeDEC}deg.fits")
@@ -41,18 +43,29 @@ for tile_sizeRA, tile_sizeDEC, Nsimu in params['tile_sizes']:
         cat = cat.to_pandas()
         dict_tile = {}
 
-        for z in z_list: 
+        for iz, z in enumerate(z_list): 
 
             Dz = dz_list[0] * n_list[0]
             Vslice = field_size / 3 * (cosmo.comoving_distance(z+Dz/2)**3-cosmo.comoving_distance(z-Dz/2)**3)
         
             rho_MS = mol_gas_density(cat.loc[cat['ISSB'] == 0], Vslice, params['alpha_co_ms'])
-            rho_SB = mol_gas_density(cat.loc[cat['ISSB'] == 1],  Vslice, params['alpha_co_sb'])
+            rho_SB = mol_gas_density(cat.loc[cat['ISSB'] == 1], Vslice, params['alpha_co_sb'])
             dict_tile[f'rho_mol_MS_at_{z}'] = rho_MS
             dict_tile[f'rho_mol_SB_at_{z}'] = rho_SB
+            tab[l,iz,0] = rho_MS
+            tab[l,iz,1] = rho_SB
     
-        dict_fieldsize[f'{l}'] = dict_tile
+        dict_fieldsize[f'tile_{l}'] = dict_tile
+
         bar.next() 
+    
+    dict_fieldsize['tile_0'][f'SB_mean'] = np.mean(tab[:,:,1], axis = (0))
+    dict_fieldsize['tile_0'][f'MS_mean'] = np.mean(tab[:,:,0], axis = (0))
+    dict_fieldsize['tile_0'][f'SB_median'] = np.median(tab[:,:,1], axis = (0))
+    dict_fieldsize['tile_0'][f'MS_median'] = np.median(tab[:,:,0], axis = (0))
+    dict_fieldsize['tile_0'][f'SB_std'] = np.std(tab[:,:,1], axis = (0))
+    dict_fieldsize['tile_0'][f'MS_std'] = np.std(tab[:,:,0], axis = (0))
+    dict_fieldsize['redshift'] = z_list
 
     bar.finish
     pickle.dump(dict, open(f"dict_dir/rhomol_alphacoMS{params['alpha_co_ms']}_alphaCOSB{params['alpha_co_sb']}_{tile_sizeRA}deg_x_{tile_sizeDEC}deg.p", 'wb'))
